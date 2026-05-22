2bb9490d7052201201b1b04256a36a81930ea89a
Griffeth Barker <github@griff.systems>
2026-05-22 14:48:21 -0700

    fix meta module loading

--- modules/ManageEngine.ServiceDesk.OnPrem/ManageEngine.ServiceDesk.OnPrem.psm1
+++ modules/ManageEngine.ServiceDesk.OnPrem/ManageEngine.ServiceDesk.OnPrem.psm1
@@ -1,11 +1,8 @@
-$modulesRoot = Split-Path $PSScriptRoot -Parent
-
 $subModules = @(
     'ManageEngine.ServiceDesk.OnPrem.Core'
     'ManageEngine.ServiceDesk.OnPrem.Requests'
 )
 
 foreach ($name in $subModules) {
-    $manifest = Join-Path $modulesRoot $name "$name.psd1"
-    Import-Module $manifest -Force -Global
+    Import-Module $name -Force -Global
 }
